0b7bd7609ae00a24ee10dfd6938684966789d60e
nguyen-hoangminh <Minh123456>
2019-12-19 18:38:42 +0900

    minor changes

--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
 sudo amazon-linux-extras install php7.2 -y
 sudo yum install php-common php-mbstring php-xml php-zip php-curl -y
-chmod 777 install-composer.sh
 ./install-composer.sh
 cp .env.example .env
 php artisan key:generate
